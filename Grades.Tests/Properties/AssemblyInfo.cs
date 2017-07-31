7156453d66a0e1291bf672fa556854ee44ea1cc9
Ryan McDougall <Ryan.McDougall@netgainhosting.com>
2017-07-31 14:55:59 -0500

    Work commit, get; set; info for properties

---
+++ Grades.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Grades.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grades.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1bffc6ff-1b74-4d19-88d8-3a0aeda40c9f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ pluralsight/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GradeBook book = new GradeBook();
+            book.Name = "Ryan's Grade Book";
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(book.Name);
+            Console.WriteLine("Average Grade: " + stats.AverageGrade);
+            Console.WriteLine("Highest Grade: " + (int)stats.HighestGrade);
+            Console.WriteLine("Lowest Grade: " + stats.LowestGrade);
+
+            WriteResult("Blah", stats.LowestGrade);
+           
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result}");
+
+        }
+    }
+}
